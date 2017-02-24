558fec9d985ee579fa75105f480e7c1d79a3696d
Jay Sanders <WyldPhyr@users.noreply.github.com>
2017-02-24 02:51:14 -0800

    Add google-trends-api

---
+++ Product/google-trends-api/examples.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var googleTrends = require('./lib/google-trends-api.min.js');
+var util = require('util');
+
+/******************** Interest over time **************************/
+
+// googleTrends.interestOverTime({keyword: 'Valentines Day'})
+// .then((res) => {
+//   console.log('this is res', res);
+// })
+// .catch((err) => {
+//   console.log('got the error', err);
+// })
+
+// googleTrends.interestOverTime({keyword: 'Valentines Day', startTime: new Date(Date.now() - (4 * 60 * 60 * 1000))}, function(err, results) {
+//   if (err) console.log('oh no error!', err);
+//   else console.log(results);
+// });
+
+
+/******************** Interest by region **************************/
+
+
+// googleTrends.interestByRegion({keyword: 'Donald Trump', startTime: new Date('2017-02-01'), endTime: new Date('2017-02-06'), resolution: 'CITY'})
+// .then((res) => {
+//   console.log(res);
+// })
+// .catch((err) => {
+//   console.log(err);
+// })
+
+// googleTrends.interestByRegion({keyword: 'Donald Trump', startTime: new Date('2017-02-01'), endTime: new Date('2017-02-06'), geo: 'US-CA'})
+// .then((res) => {
+//   console.log(res);
+// })
+// .catch((err) => {
+//   console.log(err);
+// })
+
+
+/******************** Related queries **************************/
+
+// googleTrends.relatedQueries({keyword: 'Westminster Dog Show'})
+// .then((res) => {
+//   console.log(res);
+// })
+// .catch((err) => {
+//   console.log(err);
+// })
+
+/******************** Related topics **************************/
+
+// googleTrends.relatedTopics({keyword: 'Chipotle', startTime: new Date('2015-01-01'), endTime: new Date('2017-02-10')})
+// .then((res) => {
+//   console.log(res);
+// })
+// .catch((err) => {
+//   console.log(err);
+// });
